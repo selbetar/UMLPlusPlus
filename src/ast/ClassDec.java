04121d0940010fb21e186a283d80fe5650db1f6a
Oscar Tu <o.car.w2@gmail.com>
2019-10-08 14:42:14 -0700

    Add skeleton for ClassDec

--- src/ast/ClassDec.java
+++ src/ast/ClassDec.java
@@ -1,9 +1,30 @@
 package ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassDec extends Statement {
+
+    private List<Statement> statements = new ArrayList<>();
+
     @Override
     public void parse() {
-
+        while (tokenizer.moreTokens()) {
+            Statement statement;
+            if (tokenizer.checkToken("Interface")) {
+                statement = new Interface();
+            } else if (tokenizer.checkToken("Class")) {
+                statement = new Class();
+            } else if (tokenizer.checkToken("Abstract")) {
+                String next = tokenizer.getNext();
+                if (next.equals("Class")) {
+                    statement = new AbstractClass();
+                }
+            } else {
+                // add the classname to statement
+            }
+            statements.add(statement);
+        }
     }
 
     @Override
