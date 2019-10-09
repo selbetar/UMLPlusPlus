e4b3263d683177ee62906dcae37a9634ce6db832
Oscar Tu <o.car.w2@gmail.com>
2019-10-09 11:32:25 -0700

    Implement ClassDec

--- src/ast/ClassDec.java
+++ src/ast/ClassDec.java
@@ -1,40 +1,73 @@
 package ast;
 
+import library.UmlBuilder;
+import library.exceptions.InvalidGrammarException;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ClassDec extends Statement {
 
     private List<Statement> statements = new ArrayList<>();
+    private Map<String, ArrayList> attributeMap;
+    private Map<String, ArrayList> methodMap;
+    private static ClassDec classDec;
+    private classType ctype;
+    private String className;
+    private String classAlias;
+
+    private ClassDec() {
+        this.attributeMap = new HashMap<String, ArrayList>();
+        this.methodMap = new HashMap<String, ArrayList>();
+    }
+
+    public static ClassDec getInstance() {
+        if (classDec == null) {
+            return new ClassDec();
+        }
+        return classDec;
+    }
 
     @Override
     public void parse() {
-        while (tokenizer.moreTokens()) {
-            Statement statement;
+        if (tokenizer.checkToken("Declare")) {
+            tokenizer.getNext();
             if (tokenizer.checkToken("Interface")) {
-                statement = new Interface();
+                ctype = classType.INTERFACE;
             } else if (tokenizer.checkToken("Class")) {
-                statement = new Class();
+                ctype = classType.CLASS;
             } else if (tokenizer.checkToken("Abstract")) {
-                String next = tokenizer.getNext();
-                if (next.equals("Class")) {
-                    statement = new AbstractClass();
+                tokenizer.getNext();
+                if (tokenizer.checkToken("Class")) {
+                    ctype = classType.ABSTRACT_CLASS;
                 }
-            } else {
-                // add the classname to statement
             }
-            statements.add(statement);
+            if (tokenizer.moreTokens()) {
+                className = tokenizer.getNext();
+                if (tokenizer.moreTokens()) {
+                    tokenizer.getNext();
+                    classAlias = tokenizer.getNext();
+                }
+            }
+        } else {
+            throw new InvalidGrammarException();
         }
     }
 
     @Override
     public void evaluate() {
-
+        UmlBuilder frame = UmlBuilder.getInstance();
+        frame.drawClass(ctype, className);
     }
 
     @Override
     public void nameCheck() {
-
+        if (ctype == null) {
+            throw new UnspecifiedClassTypeException();
+        }
+        // TODO check if name is in list of keywords
     }
 
     @Override
