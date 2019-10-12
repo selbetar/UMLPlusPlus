effdd37c2b01253efcbf6523d678f8e9e86096bc
sqhsb <sadeqhsb@tuta.io>
2019-10-11 23:26:54 -0700

    Support for multiple declarations

--- src/ast/ClassDec.java
+++ src/ast/ClassDec.java
@@ -3,42 +3,66 @@ package ast;
 import library.AttributeStorage;
 import library.exceptions.InvalidGrammarException;
 
+import java.util.ArrayList;
+
 public class ClassDec extends Statement {
 
     private ClassType cType;
-    private String className;
     private String classAlias;
+    private ArrayList<String> decList = new ArrayList<>();
 
 
     @Override
     public void parse() {
         tokenizer.getAndCheckNext("Declare");
-        if (tokenizer.checkToken("Interface")) {
-            cType = ClassType.INTERFACE;
-        } else if (tokenizer.checkToken("Class")) {
-            cType = ClassType.CLASS;
-        } else if (tokenizer.checkToken("Abstract")) {
+        if (tokenizer.checkToken("Interface") || tokenizer.checkToken("Class")) {
+        String type = tokenizer.getNext();
+        cType = getClassType(type);
+        String className = tokenizer.getNext();
+        parseAlias((className));
+        decList.add(className);
+        parseClasses();
+        } else if (tokenizer.checkToken("Abstract") && tokenizer.checkNextToken("Class")){
             tokenizer.getNext();
-            if (tokenizer.checkToken("Class")) {
-                cType = ClassType.ABSTRACT_CLASS;
-            }
+            tokenizer.getNext();
+            String className = tokenizer.getNext();
+            cType = getClassType("Abstract");
+            parseAlias((className));
+            decList.add(className);
+            parseClasses();
         } else {
             throw new InvalidGrammarException();
         }
-        tokenizer.getNext();
-        className = tokenizer.getNext();
-        AttributeStorage.getInstance().declaredClasses.add(className);
+        AttributeStorage.getInstance().declaredClasses.addAll(decList);
+    }
 
+    private void parseClasses() {
+        String className = "";
+        while (tokenizer.checkToken("\\,")) {
+            tokenizer.getNext();
+            className = tokenizer.getNext();
+            decList.add(className);
+            parseAlias((className));
+        }
+    }
+    private void parseAlias(String className) {
         if (tokenizer.checkToken("As")) {
             tokenizer.getNext();
             classAlias = tokenizer.getNext();
             AttributeStorage.getInstance().variableMap.put(classAlias, className);
         }
     }
+    private ClassType getClassType(String type){
+        if (type.equals("Class")) return ClassType.CLASS;
+        if (type.equals("Interface")) return ClassType.INTERFACE;
+        return ClassType.ABSTRACT_CLASS;
+    }
 
     @Override
     public void evaluate() {
-        frame.drawClass(cType, className);
+        for (String className : decList){
+            frame.drawClass(cType, className);
+        }
     }
 
     @Override
